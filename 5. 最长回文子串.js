b9e838d5cbc5133bca1ff5810cf99bd0169c0700
barretai <barretai@xiaoe-tech.com>
2021-12-28 20:13:13 +0800

    feat: 6

--- 5. 最长回文子串.js
+++ 5. 最长回文子串.js
@@ -11,11 +11,48 @@
  * 
  * 解题思路：
  * 暴力解法：
- * 伪代码：1. 穷举所有字符，保存最长的回文字符串，
+ * 伪代码：
+ * 1. 穷举所有字符，保存最长的回文字符串，
  * 2. 如何判断是否为回文字符串？
- *  a. 如果是奇数，从中间截取然后取反比较是否相等。
+ *   a. 如果是奇数，从中间截取然后取反比较是否相等。
  */
 
-var longestPalindrome = function (s) {
 
+const isSymmetryStr = (str) => {
+    const len = str.length;
+
+    for (let i = 0; i <len; i ++) {
+        if (str.charAt(i) !== str.charAt(len - i - 1)) {
+            return false;
+        }
+    }
+    return true;
 };
+
+
+var longestPalindrome = function (s) {
+    let maxStr = '';
+    let maxLen = 0;
+    
+    if (s.length <= 1) {
+        return s;
+    }
+
+    for (let i = 0, len = s.length; i < len; i++) {
+        for (let j = 1; j < len; j++) {
+            const test = s.substring(i, j + 1);
+            // console.log('-teest', i, j, test);
+            // 如果是回文字符串
+            if (isSymmetryStr(test) && test.length > maxLen) {
+                maxStr = test;
+                maxLen = test.length;
+            }
+        }
+    }
+
+    return maxStr;
+};
+
+console.log(longestPalindrome('babad'));
+// console.log(longestPalindrome('bb'))
+// console.log('bb'.substring(0, 1))
---
+++ 6. Z 字形变换.js
@@ -0,0 +1,22 @@
+/**
+ * @description z字形转换
+ * 输入：s = "PAYPALISHIRING", numRows = 3
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 
+ * 输出："PAHNAPLSIIGYIR"
+ * 输入：s = "PAYPALISHIRING", numRows = 4
+ * 输出："PINALSIGYAHRPI"
+ * 解释：
+    P     I    N
+    A   L S  I G
+    Y A   H R
+    P     I
+ */
+
+var convert = function (s, numRows) {
+
+};
+
+console.log(convert('PAYPALISHIRING'));
